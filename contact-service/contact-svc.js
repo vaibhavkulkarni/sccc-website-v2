95a422e8fa3ad3a93b8174242162ee8bb9969d0b
Vaibhav <vaibhz@gmail.com>
2023-05-06 14:18:18 +0100

    Corrections...

--- contact-service/contact-svc.js
+++ contact-service/contact-svc.js
@@ -57,7 +57,7 @@ app.listen(7000, function () {
 
 function sendEmail(data, res) {
   let email = { from: process.env.EMAIL_USER, to: process.env.EMAIL_USER };
-  email.subject = "Contact reuqest: " + data.subject;
+  email.subject = "Contact request: " + data.subject;
 
   const output = `
         <p>Hello,<p>
